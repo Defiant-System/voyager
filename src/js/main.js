b7c678634dd1a0f34eab1a6da349138b518cbc9a
Hakan Bilgin <hbi99@hotmail.com>
2024-04-05 16:41:27 +0200

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+
+@import "./modules/test.js"
+
+
+const voyager = {
+	init() {
+		// fast references
+		this.content = window.find("content");
+
+		// DEV-ONLY-START
+		Test.init(this);
+		// DEV-ONLY-END
+	},
+	dispatch(event) {
+		switch (event.type) {
+			// system events
+			case "window.init":
+				break;
+			// custom events
+			case "open-help":
+				karaqu.shell("fs -u '~/help/index.md'");
+				break;
+		}
+	}
+};
+
+window.exports = voyager;
