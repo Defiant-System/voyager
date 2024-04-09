b16339a64e5584c01a61ba835f4b1bdc246bd851
Hakan Bilgin <hbi99@hotmail.com>
2024-04-09 20:27:28 +0200

    Update reel.js

--- src/js/classes/reel.js
+++ src/js/classes/reel.js
@@ -22,11 +22,31 @@ class Reel extends Item {
 	}
 
 	go(which) {
-		this.transform.rotate.y = 15;
+		let time = Date.now(),
+			start = this.transform.rotate.y,
+			end = start + (which === "next" ? 90 : -90),
+			duration = 1000;
+		console.log({ time, duration, start, end });
+		this.anim = { time, duration, start, end };
 	}
 
 	update() {
-		this.childs[0].preview();
+		if (this.anim) {
+			let t = Date.now() - this.anim.time,
+				b = this.anim.start,
+				c = this.anim.end,
+				d = this.anim.duration,
+				value = Math.floor(Math.tween.easeInOut(t, b, c, d));
+			
+			if (value >= this.anim.end) {
+				value = this.anim.end;
+				delete this.anim;
+			}
+
+			this.transform.rotate.y = value;
+		} else {
+			this.childs[0].preview();
+		}
 
 		// this.childs.map(e => e.preview());
 	}
